927fe36bbb68e89bf8fe63dd6e4b06462715b600
unknown <ahleet@buffalo.edu>
2018-11-10 12:23:03 -0500

    with graph!

--- climate.R
+++ climate.R
@@ -1,9 +1,5 @@
 library(FedData)
 library(rnoaa)
-?ghcnd
-ghcnd_stations(stationid = "USW00023183")
-ghcnd_stations(ACW00023183)
-ghcnd_search("USW00023183")
 ghcnd_search("USW00023183", date_min = "2018-07-01", date_max = "2018-07-31", var = "all")
 Tmax_PHX <- ghcnd_search("USW00023183", date_min = "2018-07-01", date_max = "2018-07-31", var = "TMAX")
 library(ggplot2)
@@ -13,14 +9,18 @@ library(dplyr)
 library(sf)
 library(spData)
 library(tidyverse)
-install.packages("weathermatics")
+
 
 # first try
 
 Tmax_PHX_df <- as.data.frame(Tmax_PHX) 
 Tmax_PHX_mutate <- Tmax_PHX_df %>% 
   mutate(tmax = tmax.tmax/10)
-plot(Tmax_PHX_mutate$tmax.date, Tmax_PHX_mutate$tmax)
+plot(Tmax_PHX_mutate$tmax.date, Tmax_PHX_mutate$tmax,
+     xlab="July 2018",
+     ylab="Maximum Temperature Phoenix, AZ Airport Station",
+     main="Maximum Temperature by day",
+     col="red")
 
 
 
